ddfe0fa91c1d2a9f9ac7534b78fe7239ce20f920
kyuuuuu <mkmk0607@gmail.com>
2023-08-08 16:12:56 +0900

    Revert "Revert "0808 수업내용 추가""
    This reverts commit 15c01026e7cb8d30c6f72aaaf07341bf7e843837.

---
+++ 01_Java/a10_collection/src/edu/kh/collection/list/ex/ListExRun.java
@@ -0,0 +1,19 @@
+package edu.kh.collection.list.ex;
+
+public class ListExRun {
+	public static void main(String[] args) {
+		
+		ListEx main = new ListEx();
+		main.ex1();
+//		main.ex2();
+//		main.ex3();
+//		main.ex4();
+//		main.ex5();
+//		main.ex6();
+//		main.ex7();
+//		main.ex8();
+//		main.ex9();
+//		main.ex10();
+		
+	}
+}
