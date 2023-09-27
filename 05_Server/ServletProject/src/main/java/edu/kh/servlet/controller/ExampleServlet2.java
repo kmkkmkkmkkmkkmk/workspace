8179ae137c09a2504d54168b3e91a248473b7260
kyuuuuu <mkmk0607@gmail.com>
2023-09-27 16:06:05 +0900

    0927 수업자료

--- 05_Server/ServletProject/src/main/java/edu/kh/servlet/controller/ExampleServlet2.java
+++ 05_Server/ServletProject/src/main/java/edu/kh/servlet/controller/ExampleServlet2.java
@@ -3,6 +3,7 @@ package edu.kh.servlet.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -29,7 +30,22 @@ import jakarta.servlet.http.HttpServletResponse;
 public class ExampleServlet2 extends HttpServlet{
 
 	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("ExampleServlet2 객체 생성");
+	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("ExampleServlet2 객체 파괴");
+		
+	}
+	
+	
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+//		System.out.println("코드 수정됨");
+		
 		
 		// String req.getParameter("name속성") : 
 		// - 제출된 input 값 중 name이 일치하는 input의 value 얻어오기
