55e91b8c21dd66dde0506fb121fb22406d98fa1b
kyuuuuu <mkmk0607@gmail.com>
2023-10-18 09:07:57 +0900

    Revert "Revert "1017 수업자료""
    This reverts commit 7ad929fecaf43f952dba8befe62cadbfee845948.

--- 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/member/model/mapper/MemberMapper.java
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/member/model/mapper/MemberMapper.java
@@ -29,5 +29,15 @@ import edu.kh.project.member.model.dto.Member;
 @Mapper
 public interface MemberMapper {
 	
+	/** 로그인
+	 * @param inputMember
+	 * @return loginMember
+	 */
 	Member login(Member inputMember);
+
+	/** 회원 가입
+	 * @param inputMember
+	 * @return result
+	 */
+	int signup(Member inputMember);
 }
