4b2200d3f455510e175e16a807f70a8bf77a7eae
kyuuuuu <mkmk0607@gmail.com>
2023-10-19 15:13:09 +0900

    1019 수업자료

--- 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/admin/model/service/AdminService.java
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/admin/model/service/AdminService.java
@@ -1,5 +1,7 @@
 package edu.kh.project.admin.model.service;
 
+import java.util.List;
+
 import edu.kh.project.member.model.dto.Member;
 
 public interface AdminService {
@@ -9,5 +11,34 @@ public interface AdminService {
 	 * @return selectMember
 	 */
 	Member selectMember(String inputEmail);
+
+	/** 전체 회원 조회
+	 * @return memberList
+	 */
+	List<Member> selectAll();
+
+	/** 전체 회원 조회(+정렬)
+	 * @param sort
+	 * @return memberList
+	 */
+	List<Member> selectSort(int sort);
+
+	/** 회원 탈퇴 복구
+	 * @param memberNo
+	 * @return result
+	 */
+	int restoration(int memberNo);
+
+	/** 권한 변경
+	 * @param memberNo
+	 * @return result
+	 */
+	int changeAuthority(int memberNo);
+
+	/** 비밀번호 초기화
+	 * @param memberNo
+	 * @return result
+	 */
+	int initPw(int memberNo);
 
 }
