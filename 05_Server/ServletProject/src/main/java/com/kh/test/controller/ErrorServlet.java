cab99cb43e1b5f821f43314cf20a241071ed0e68
kmkkmkkmkkmkkmk laptop <mkmk0607@gmail.com>
2023-10-24 18:16:15 +0900

    커밋

---
+++ 05_Server/ServletProject/src/main/java/com/kh/test/controller/ErrorServlet.java
@@ -0,0 +1,27 @@
+package com.kh.test.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/errorPage")
+public class ErrorServlet extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.setCharacterEncoding("UTF-8");
+
+		String blackPen = req.getParameter("blackPen");
+		String redPen = req.getParameter("redPen");
+		String bluePen = req.getParameter("bluePen");
+		String white = req.getParameter("white");
+		
+		System.out.println();
+	}
+	
+}
