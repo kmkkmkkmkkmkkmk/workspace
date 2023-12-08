5d026fe06d9d38be2ee5b67d07a5e3e57f4aece3
kyuuuuu <mkmk0607@gmail.com>
2023-12-08 09:32:56 +0900

    1207 수업자료

--- react-workspace/my-app/src/App.js
+++ react-workspace/my-app/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
 // import 여기서사용할이름 from './js파일경로';
-import ClassComponent from './components/R01_ClassComponent';
+// import ClassComponent from './components/R01_ClassComponent';
+// import FunctionComponent from './components/R02_FunctionComponent';
 
-import FunctionComponent from './components/R02_FunctionComponent';
+import Props1 from './components/R03_Props';
+import Props2 from './components/R04_Props2';
+import Props3 from './components/R05_Props3';
+
+
+import State1 from './components/R06_State1';
 
 /*  JSX  <= JS XML */
 
@@ -13,21 +19,37 @@ import FunctionComponent from './components/R02_FunctionComponent';
    - 여러 요소를 반환하고 싶을 때 부모 요소로 묶어준다!
 */
 /* ======================================================== */
-function App() {
+function App() {  /* 감싸는 게 부모 */
   return (
     /* <></> (fragment, 조각) : 반환되는 요소를 감싸는 태그, 해석 X */
     <>
       <h1>테스트</h1>
-      <ClassComponent/>
+      {/* <ClassComponent/> */} {/* 감싸진 게 자식 */}
       
       {/* jsx 주석 */}
       {/* <ClassComponent/> */}
       
       <hr/>
 
-      <FunctionComponent/>
+      {/* <FunctionComponent/> */}
       {/* <FunctionComponent/> */}
 
+      <hr/>
+{/* 
+      <Props1/>
+      <Props1 name='홍길동' num='1'/>
+      <Props1 name='김철수' num='2'/>
+
+      <hr/>
+
+      <Props2 name='강감찬' age='72' address='낙성대' gender='남자'/>
+      <Props2 name='천추태후' age='66' address='개경' gender='여자'/>
+
+      <hr/>
+      <Props3 name='김민규'/>
+      <Props3 name='윤성국'/>
+ */}
+    <State1/>
     </>
   );
   
---
+++ react-workspace/my-app/src/components/R04_Props2.js
@@ -0,0 +1,52 @@
+import React from 'react';  /* imr 작성하여 자동완성 */
+
+const PropsEx2 = (props) => {
+
+  // props라는 객체에서 
+  // {} 내부에 작성된 변수명과 같은 key가 존재하면
+  // 변수에 value를 대입
+  let {name, age, address, gender} = props;
+
+  return(
+    /* className(React표기법) == class(HTML표기법) */
+    <ul className='info'>
+      <li>이름 : {name}</li>
+      <li>나이 : {age}</li>
+      <li>주소 : {address}</li>
+      <li>성별 : {gender}</li>
+    </ul>
+  ); 
+
+}
+
+export default PropsEx2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
